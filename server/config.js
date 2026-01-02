2952f52ec767fbfc2f7992cc88b33acbdb77fbc7
Lorenzo Mevellec <l.mevellec@gmail.com>
2026-01-02 18:33:31 +0100

    feat: udating port variable name

--- server/config.js
+++ server/config.js
@@ -19,4 +19,4 @@ export const auth0Audience = audience;
 export const auth0Domain = domain;
 export const auth0Issuer = `https://${domain}/`;
 export const basePath = resolveBasePath(audience);
-export const port = process.env.SERVER_PORT || 3000;
+export const port = process.env.PORT || 3000;
